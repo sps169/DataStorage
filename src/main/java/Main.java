288eedd1ff7a01095f5e704e0b5c8b0e9111d538
Sergio <sps169@outlook.es>
2021-09-24 11:39:33 +0200

    Functional Generation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-	public static void main(String[] args) {
+import Data.Character;
+import Data.Weapon;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+	public static List fillCharacterCollection(Collection weaponCollection, int n)
+	{
+		List<Character> list = new LinkedList<>();
+		for (short i = 0; i < n; i++)
+		{
+			list.add(new Character(i, "Personaje " + (i + 1), (Weapon)weaponCollection.toArray()[(int)(Math.random()*weaponCollection.size())]));
+		}
+		return list;
+	}
+
+	public static List fillWeaponCollection(int n)
+	{
+		List<Weapon> list = new LinkedList<>();
+		for (short i = 0; i < n; i++)
+		{
+			list.add(new Weapon(i, "Arma " + (i + 1), (int)(Math.random()*10)));
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		List<Weapon> weapons = fillWeaponCollection(40);
+		List<Character> characters = fillCharacterCollection(weapons, 200);
+		characters.stream().filter(c -> {return c.getWeapon().getAtk() <= 3;}).forEach(c-> System.out.println(c));
 	}
 }
